3f21a560fbbd11cf1cdd5f8912da162ecda0df42
Will <43840962+willn05@users.noreply.github.com>
2023-12-27 15:48:15 +1100

    SPHYCNX HSA BEsLLED US

--- piston.gd
+++ piston.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var animating = false
 var bruh = false
-var launch_body: CharacterBody2D
+var launch_body = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,7 +10,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	#print(bruh)
 	
 	if Input.is_action_just_pressed("ui_down"):
@@ -20,7 +20,7 @@ func _process(delta):
 	if animating:
 		var y = 1.75 * delta
 		$Spring.scale.y -= y
-		$Piston.position.y -= 1260*delta
+		$Piston.position.y -= 1200*delta #1260
 		
 		if $Spring.scale.y <= -0.22:
 			animating = false
@@ -31,17 +31,23 @@ func _process(delta):
 	
 	if bruh and launch_body != null:
 		print(launch_body)
-		launch_body.velocity.y = -1000
+		if launch_body is CharacterBody2D:
+			launch_body.velocity.y = -1000
+		elif launch_body is RigidBody2D:
+			
+			for child in launch_body.get_parent().get_children():
+				if child is RigidBody2D:
+					child.apply_impulse(Vector2(0, -2500))
+					child.apply_force(Vector2(0, -2500))
+			
 		bruh = false
 
 
+
+
 func _on_area_2d_body_entered(body):
-	
-	if body is CharacterBody2D:
-		launch_body = body
+	launch_body = body
 		
 
-
 func _on_area_2d_body_exited(body):
-	if body is CharacterBody2D:
-		launch_body = null
+	launch_body = null
