6bbbd6ed0755101db9140c989ab64151e338b574
idrinkradium <purple.ninja.boy@gmail.com>
2023-12-26 20:47:59 -0500

    newfilesshitt

--- Camera2D.gd
+++ Camera2D.gd
@@ -7,14 +7,14 @@ var zoomfactor=0.0
 func _process(delta):
 	
 	#print(n)
-	if Input.is_action_just_released("Zoom In"):
+	if Input.is_action_just_released("Zoom In") and zoomfactor<3:
 		zoomfactor+=1
 		n = pow(1.5,zoomfactor)
 		zoom = Vector2(n, n)
 		
 		print(zoom)
 		
-	if Input.is_action_just_released("Zoom Out"):
+	if Input.is_action_just_released("Zoom Out") and zoomfactor>-2:
 		zoomfactor-=1
 		n = pow(1.5,zoomfactor)
 		zoom = Vector2(n, n)
@@ -22,6 +22,9 @@ func _process(delta):
 		
 	if Input.is_action_pressed("Camera Pan"):
 		offset-=mouse_delta/zoom
+		print(mouse_delta/zoom)
+	
+	mouse_delta = Vector2.ZERO
 		
 	
 func _input(event):
