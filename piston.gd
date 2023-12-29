ea6120ce4fd1188fc14bb699019feb64ae509d10
Will <43840962+willn05@users.noreply.github.com>
2023-12-29 11:43:01 +1100

    p

--- piston.gd
+++ piston.gd
@@ -2,9 +2,7 @@ extends Node2D
 
 @export var height = 150
 @export var animation_duration = 0.1
-
-var animating = false
-var bruh = false
+@export var velocity = 1000
 
 var power = false
 
@@ -15,36 +13,14 @@ func _physics_process(delta):
 		power_changed.emit(false)
 	if Input.is_action_just_pressed("ui_up"):
 		power_changed.emit(true)
-	#if bruh and launch_body != null:
-		#print(launch_body)
-		#if launch_body is CharacterBody2D:
-			#launch_body.velocity.y = -1000
-		#elif launch_body is RigidBody2D:
-			#
-			#for child in launch_body.get_parent().get_children():
-				#if child is RigidBody2D:
-					#child.apply_impulse(Vector2(0, -2500))
-					#child.apply_force(Vector2(0, -2500))
-			#
-		#bruh = false
 		
-	#if animating:
-		#var y = 1.75 * delta
-		#$Spring.scale.y -= y
-		#$Piston.position.y -= 1260*delta #1260
-		#
-		#if $Spring.scale.y <= -0.22:
-			#animating = false
-			#
-	#if Input.is_action_just_pressed("ui_home"):
-		#$Spring.scale.y = 0
-		#$Piston.position.y = -20
-	
 
 func _on_power_changed(new_power):
+	if power == new_power:
+		return
+		
 	power = new_power
-	animating = true
-	bruh = true
+	
 	if power:
 		$PistonOut.play()
 	else:
@@ -56,4 +32,13 @@ func _on_power_changed(new_power):
 	
 	new_y = -.0014 * height if power else 0
 	tween.parallel().tween_property($Spring, "scale", Vector2($Spring.scale.x, new_y), animation_duration)
-	
+
+	if power:
+		for collision in $Piston/ShapeCast2D.collision_result:
+			if collision.collider is CharacterBody2D:
+				collision.collider.velocity.y = -velocity
+			elif collision.collider is RigidBody2D:
+				for child in collision.collider.get_parent().get_children():
+					if child is RigidBody2D:
+						child.apply_impulse(Vector2(0, -velocity))
+						child.apply_force(Vector2(0, -velocity))
