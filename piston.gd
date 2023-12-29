3ce69e658a6beeec7bbaed006c3fa92bcf90df28
Will <43840962+willn05@users.noreply.github.com>
2023-12-29 14:40:49 +1100

    stuff

--- piston.gd
+++ piston.gd
@@ -3,13 +3,13 @@ extends Node2D
 @export var height = 150
 @export var animation_duration = 0.1
 @export var player_velocity = 1000
-@export var ragdoll_force = 100
+@export var ragdoll_force = 300
 
-var power = false
+var powered = false
 
 signal power_changed(new_power: bool)
 
-func _physics_process(delta):
+func _process(delta):
 	if Input.is_action_just_pressed("ui_down"):
 		power_changed.emit(false)
 	if Input.is_action_just_pressed("ui_up"):
@@ -17,29 +17,43 @@ func _physics_process(delta):
 		
 
 func _on_power_changed(new_power):
-	if power == new_power:
+	if powered == new_power:
 		return
 		
-	power = new_power
+	powered = new_power
 	
-	if power:
+	if powered:
 		$PistonOut.play()
 	else:
 		$PistonIn.play()
 		
+	#$Piston/AnimatableBody2D/CollisionShape2D.disabled = true
+				
+			## launch ragdoll
+			#if collision.collider is RigidBody2D:
+				#for child in collision.collider.get_parent().get_children():
+					#if not child is RigidBody2D:
+						#continue
+					#
+					#child.apply_impulse(Vector2(0, -ragdoll_force))
+					#child.apply_force(Vector2(0, -ragdoll_force))
+
+	
 	var tween = get_tree().create_tween()
-	var new_y = $Piston.position.y - height if power else $Piston.position.y + height
+	var new_y = $Piston.position.y - height if powered else $Piston.position.y + height
 	tween.parallel().tween_property($Piston, "position", Vector2($Piston.position.x, new_y), animation_duration)
 	
-	new_y = -.0014 * height if power else 0
+	new_y = -.0014 * height if powered else 0
 	tween.parallel().tween_property($Spring, "scale", Vector2($Spring.scale.x, new_y), animation_duration)
+	
+	var finished = func test():
+		if powered:
+			for collision in $Piston/ShapeCast2D.collision_result:
+				# launch player
+				if collision.collider is CharacterBody2D:
+					#collision.collider.velocity.y += -25
+					continue
 
-	if power:
-		for collision in $Piston/ShapeCast2D.collision_result:
-			if collision.collider is CharacterBody2D:
-				collision.collider.velocity.y = -player_velocity
-			elif collision.collider is RigidBody2D:
-				for child in collision.collider.get_parent().get_children():
-					if child is RigidBody2D:
-						child.apply_impulse(Vector2(0, -ragdoll_force))
-						child.apply_force(Vector2(0, -ragdoll_force))
+	tween.finished.connect(finished)
+	
+
