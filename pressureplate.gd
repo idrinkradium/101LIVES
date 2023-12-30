83cdf421a85ca37cbdd6d54ed6b6ab40284db09f
Will <43840962+willn05@users.noreply.github.com>
2023-12-30 11:38:18 +1100

    fdsdff

--- pressureplate.gd
+++ pressureplate.gd
@@ -4,13 +4,22 @@ extends Node2D
 
 var powered = false
 var previously_powered = false
+var tween: Tween
+var busy = false
 
-func _physics_process(delta): 
+func _physics_process(delta):
+	if busy: 
+		return
+		 
 	previously_powered = powered
-	powered = !$StaticBody2D/ShapeCast2D.collision_result.is_empty()
+	powered = !$ShapeCast2D.collision_result.is_empty()
 	
 	if previously_powered != powered:
-		var tween = create_tween()
+		
+		if tween:
+			tween.kill()
+		tween = create_tween()
+		
 		var animation_duration = 0.1
 		if powered:
 			$PressurePlateIn.play()
@@ -20,6 +29,10 @@ func _physics_process(delta):
 			$PressurePlateOut.play()
 			tween.parallel().tween_property($StaticBody2D, "scale", Vector2($StaticBody2D.scale.x, 0.2), animation_duration)
 			tween.parallel().tween_property($StaticBody2D, "position", Vector2($StaticBody2D.position.x, $StaticBody2D.position.y - 6), animation_duration)
+		
+		var finished = func():
+			busy = false
+		tween.finished.connect(finished)
 		
 		await get_tree().create_timer(0.12).timeout
 		for powerable in powerables:
