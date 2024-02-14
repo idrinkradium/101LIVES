e614be94f9143553fb77843963f9f8577534b842
Will <43840962+willn05@users.noreply.github.com>
2024-02-14 16:36:07 +1100

    yaay

--- fadeable.gd
+++ fadeable.gd
@@ -1,17 +1,19 @@
-class_name Fadable extends Node2D
+class_name Fadable extends CanvasItem
 
-var time = 0
-var should_fade = true
+@onready var player = get_tree().current_scene.get_node("Player")
+@export var should_fade = true
 @export var distance_object = self 
 @export var far= 200.0
 @export var spread=20.0
-@export var wait = 0
-var wait_time = 0.0
+@export var wait_time = 0
 @export var wait_distance = 500
-@onready var player =  get_tree().current_scene.get_node("Player")
+var wait_counter = 0.0
+var f = 1
 
 func _ready():
 	modulate.a = 0
+	if wait_time > 0:
+		f = 0
 	
 func _process(delta):
 	if !should_fade:
@@ -19,14 +21,21 @@ func _process(delta):
 
 	var distance = abs(player.position.x - distance_object.position.x)
 	
-	if wait > 0 and wait_distance > distance:
-		return
-	
-	if wait_distance < distance:
-		wait_time += delta
+	#print(" f:", f, " wait_distance:", wait_distance, " wait_counter:", wait_counter, " distance:", distance)
+	if wait_time > 0:
+		if wait_distance < distance:
+			return
+			
+		wait_counter += delta
+		
+		if wait_counter < wait_time:
+			return
+		
+		f += delta
+		f = min(f, 1)
 	
 	if distance < far:
-		modulate.a = 1
+		modulate.a = 1 * f
 	else:
-		modulate.a = (-spread/100000)*((distance-far)**2)+1
+		modulate.a = ((-spread/100000)*((distance-far)**2)+1) * f
 
