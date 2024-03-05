185525b8e5b59d387dacca744f13fd6c02dfabbf
idrinkradium <purple.ninja.boy@gmail.com>
2024-03-04 22:02:04 -0500

    level5 done

--- level5.gd
+++ level5.gd
@@ -11,6 +11,9 @@ func _on_fire_timer_timeout():
 		$FireRight.on = fire_toggle
 	if !$PressurePlateLeft.powered:
 		$FireLeft.on = !fire_toggle
+	if $PressurePlateLeft.powered and $PressurePlateRight.powered:
+		$Door.power_changed.emit(true)
+	else: $Door.power_changed.emit(false)
 
 func _on_belt_powerable_power_changed(new_power):
 	$BeltMaster.speed = -$BeltMaster.speed
