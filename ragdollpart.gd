a464ce442570a71cc721ca89d79cbce09c6fdb09
Will <43840962+willn05@users.noreply.github.com>
2024-06-25 11:00:36 +1000

    ex

--- ragdollpart.gd
+++ ragdollpart.gd
@@ -1,6 +1,13 @@
 extends Node2D
 class_name Limb
 var hp = 100
+@export var destroyparticles : CPUParticles2D
 func _process(delta):
 	if position.y > 2000 or hp <= 0:
+		var p = destroyparticles.duplicate()
+		p.emitting=true
+		p.position = global_position
+		get_tree().root.get_node("Game").add_child(p)
+		
+		# delet
 		queue_free()
