9cc7a2317a9619efe37c5376b38d1ea93715f9de
idrinkradium <purple.ninja.boy@gmail.com>
2024-06-25 20:51:16 -0400

    awdasd

--- rigidshapes.gd
+++ rigidshapes.gd
@@ -3,15 +3,15 @@ extends Node2D
 @export var line_thickness = 2.8
 @export var line_color = Color("ffffff")
 func _ready():
-	$RigidBody2D/rigidshapes.color = Color.BLACK
+	$RigidBody2D/outline.color = Color.BLACK
 
 	
 	var between = $RigidBody2D.position - $centerofmass.position
 	var i= 0
-	for position in $RigidBody2D/rigidshapes.polygon:
-		$RigidBody2D/rigidshapes.polygon[i] += between
+	for position in $RigidBody2D/outline.polygon:
+		$RigidBody2D/outline.polygon[i] += between
 		i +=1
 		
-	$RigidBody2D/CollisionPolygon2D.polygon = $RigidBody2D/rigidshapes.polygon
-	$RigidBody2D/outline.polygon = Geometry2D.offset_polygon($RigidBody2D/rigidshapes.polygon, -line_thickness)[0]
+	$RigidBody2D/CollisionPolygon2D.polygon = $RigidBody2D/outline.polygon
+	$RigidBody2D/rigidshapes.polygon = Geometry2D.offset_polygon($RigidBody2D/outline.polygon, -line_thickness)[0]
 
