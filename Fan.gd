fa9d710a6ec1de451eab4e427e4eeb188a1fb8b0
idrinkradium <purple.ninja.boy@gmail.com>
2024-06-28 04:06:42 -0400

    awda

--- Fan.gd
+++ Fan.gd
@@ -9,10 +9,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if powered == true:
+		$StaticBody2D/AnimatedSprite2D.play("fan")
+	else:
+		$StaticBody2D/AnimatedSprite2D.stop("fan")
 
 func _on_power_changed(new_power):
 	if powered == new_power:
 		return
 	
 	powered = new_power
+	if powered == true:
+		$AnimatedSprite2D.play("fan")
+	else:
+		$AnimatedSprite2D.stop("fan")
