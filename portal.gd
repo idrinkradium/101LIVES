8ebd381d0c37e39993dec61819ddc78b2953e9ad
idrinkradium <purple.ninja.boy@gmail.com>
2024-06-29 20:12:34 -0400

    awdawsdawd

--- portal.gd
+++ portal.gd
@@ -1,12 +1,24 @@
 extends Node2D
 @export var bluetrue = false
 @export var sync = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+@export var portal : Node2D
+func _physics_process(delta):
 	pass
+func _ready():
+	if bluetrue == true:
+		$Portalblue.visible = true
+		$Portalorange.visible =false
+	else: 
+		$Portalblue.visible = false
+		$Portalorange.visible = true
+func _on_area_2d_body_entered(body):
+	if body is CharacterBody2D:
+		body.velocity = -Vector2(body.velocity)
+		body.position = portal.position + Vector2(0,-70)
+		#print(body.velocity)
+	if body is RigidBody2D:
+		print("awdasd")
+		body.position = portal.position + Vector2(0,-200)
+		body.linear_velocity = -Vector2(body.linear_velocity)
+		print(body.linear_velocity)
+		#print(body)
