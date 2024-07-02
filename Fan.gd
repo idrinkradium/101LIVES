b154e2931b213e9c9a071737803ccc0b7ce37e99
idrinkradium <purple.ninja.boy@gmail.com>
2024-07-01 23:38:04 -0400

    awdasdad

--- Fan.gd
+++ Fan.gd
@@ -1,6 +1,6 @@
 extends Powerable
 
-
+@export var power = 980
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,17 +9,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$StaticBody2D/Area2D.gravity = power
 	if powered == true:
 		$StaticBody2D/AnimatedSprite2D.play("fan")
 	else:
-		$StaticBody2D/AnimatedSprite2D.stop("fan")
-
+		$StaticBody2D/AnimatedSprite2D.stop()
+	$StaticBody2D/Area2D.gravity_direction.x = sin((1.0/180.0)*PI*self.rotation_degrees)
+	$StaticBody2D/Area2D.gravity_direction.y = -cos((1.0/180.0)*PI*self.rotation_degrees)
+	$StaticBody2D/AnimatedSprite2D.speed_scale = (power/980.0) *2
 func _on_power_changed(new_power):
 	if powered == new_power:
 		return
 	
 	powered = new_power
 	if powered == true:
-		$AnimatedSprite2D.play("fan")
+		$StaticBody2D/AnimatedSprite2D.play("fan")
 	else:
-		$AnimatedSprite2D.stop("fan")
+		$StaticBody2D/AnimatedSprite2D.stop("fan")
